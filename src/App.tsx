3278d2c93083f1aca14fbe4a2e0ae28ad55fb919
若非 <rf.wangchn@foxmail.com>
2022-03-02 18:20:58 +0800

    引入 monaco-editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,47 @@
-import React from "react";
-
-import BasicLayout from "./layouts/basicLayout";
-import Slide from "./components/slide";
-import Header from "./components/header";
-import EditorContent from "./components/editorContent";
+import React, { useEffect } from "react";
+import { editor } from "monaco-editor";
+import githubTheme from "monaco-themes/themes/GitHub.json";
 
 import styles from "./App.module.scss";
 
+import type { IKeyboardEvent } from "monaco-editor";
+
 const App: React.FC = () => {
+  useEffect(() => {
+    const instance = editor.create(
+      document.querySelector(`.${styles.editorInner}`) as HTMLDivElement,
+      {
+        language: "markdown",
+        lineNumbers: "off",
+        roundedSelection: false,
+        minimap: {
+          enabled: false,
+        },
+      }
+    );
+    editor.defineTheme("github", githubTheme as any);
+    instance.onKeyUp((keyUpEvent: IKeyboardEvent) => {
+      const target = keyUpEvent.target as HTMLInputElement;
+      target.setAttribute("value", "test");
+      console.log(instance.getPosition(), target.value, instance.getValue());
+    });
+  }, []);
+
   return (
-    <BasicLayout>
-      <Slide />
-      <div className={styles.contentContainer}>
-        <Header />
-        <EditorContent />
+    <div className={styles.App}>
+      <div className={styles.sidebar}>
+        <div className={styles.sidebarInner}>Slide</div>
       </div>
-    </BasicLayout>
+      <div className={styles.content}>
+        <div className={styles.header}>Header</div>
+        <div className={styles.pageTitle}>
+          <div className={styles.pageTitleInner}>Page Title</div>
+        </div>
+        <div className={styles.editor}>
+          <div className={styles.editorInner} />
+        </div>
+      </div>
+    </div>
   );
 };
 
