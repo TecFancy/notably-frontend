59be9dc6e405c46bdaed5d90388c664afc110c19
若非 <rf.wangchn@foxmail.com>
2022-03-04 15:58:46 +0800

    Merge branch 'feature/f20220324' of github.com:smpower/notably-frontend into feature/f20220324

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { EditorState } from "@codemirror/state";
+import { EditorState, Facet } from "@codemirror/state";
 import { history, historyKeymap } from "@codemirror/history";
 import { defaultKeymap } from "@codemirror/commands";
 import { markdown } from "@codemirror/lang-markdown";
@@ -8,11 +8,17 @@ import { defaultHighlightStyle } from "@codemirror/highlight";
 import { indentOnInput } from "@codemirror/language";
 import { highlightActiveLineGutter } from "@codemirror/gutter";
 import { bracketMatching } from "@codemirror/matchbrackets";
-import { highlightSelectionMatches, searchKeymap } from "@codemirror/search";
+import {
+  findPrevious,
+  highlightSelectionMatches,
+  searchKeymap,
+} from "@codemirror/search";
 import { autocompletion, completionKeymap } from "@codemirror/autocomplete";
 import { commentKeymap } from "@codemirror/comment";
 import { rectangularSelection } from "@codemirror/rectangular-selection";
 import { lintKeymap } from "@codemirror/lint";
+import { Range, RangeSetBuilder } from "@codemirror/rangeset";
+import { Line } from "@codemirror/text";
 import {
   EditorView,
   highlightSpecialChars,
@@ -20,15 +26,66 @@ import {
   dropCursor,
   highlightActiveLine,
   keymap,
+  ViewPlugin,
+  ViewUpdate,
+  DecorationSet,
+  Decoration,
 } from "@codemirror/view";
 
 import styles from "./App.module.scss";
 
+const textStartsWith = [
+  {
+    startsWith: "# ",
+    decoLine: Decoration.line({ class: styles.heading1 }),
+    callback: (deco: Range<Decoration>[], decoration: Decoration, line: Line) =>
+      deco.push(decoration.range(line.from)),
+  },
+  {
+    startsWith: "## ",
+    decoLine: Decoration.line({ class: styles.heading2 }),
+    callback: (deco: Range<Decoration>[], decoration: Decoration, line: Line) =>
+      deco.push(decoration.range(line.from)),
+  },
+];
+
 const App: React.FC = () => {
   useEffect(() => {
     const editorState = EditorState.create({
-      doc: "# Heading 1.",
+      doc: "# Heading 1.\n# Heading 12.\n## Heading 2.\nhaha\n### Heading 3.\njojo\n### Heading 4.",
       extensions: [
+        ViewPlugin.fromClass(
+          class {
+            decorations: DecorationSet;
+            constructor(view: EditorView) {
+              this.decorations = this.getDeco(view);
+            }
+            update(update: ViewUpdate) {
+              if (update.docChanged || update.selectionSet) {
+                this.decorations = this.getDeco(update.view);
+              }
+            }
+            getDeco(view: EditorView) {
+              const deco: Range<Decoration>[] = [];
+              for (const { from, to } of view.visibleRanges) {
+                for (let pos = from; pos <= to; ) {
+                  let line = view.state.doc.lineAt(pos);
+                  textStartsWith.forEach((item) => {
+                    if (line.text.startsWith(item.startsWith)) {
+                      item.callback(deco, item.decoLine, line);
+                    }
+                  });
+                  pos = line.to + 1;
+                }
+              }
+              return Decoration.set(deco);
+            }
+          },
+          {
+            decorations: (v) => v.decorations,
+          }
+        ),
+        // zebraStripes(),
         highlightActiveLineGutter(),
         highlightSpecialChars(),
         history(),
