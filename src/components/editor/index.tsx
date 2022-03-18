9212ce5e3e203ffae457ca6ba9d9bbf1fc97419e
若非 <rf.wangchn@foxmail.com>
2022-03-18 18:20:59 +0800

    chore: 极简 md 编辑器

--- src/components/editor/index.tsx
+++ src/components/editor/index.tsx
@@ -1,21 +1,42 @@
 import { useEffect } from "react";
 import { EditorState } from "@codemirror/state";
-import { EditorView, keymap, ViewPlugin } from "@codemirror/view";
+import {
+  Decoration,
+  DecorationSet,
+  EditorView,
+  keymap,
+  ViewPlugin,
+  ViewUpdate,
+} from "@codemirror/view";
 import { defaultKeymap } from "@codemirror/commands";
 import { history, historyKeymap } from "@codemirror/history";
 import { markdown, markdownKeymap } from "@codemirror/lang-markdown";
 
 import styles from "./index.module.scss";
 
+const addHeading12md = ViewPlugin.fromClass(
+  class {
+    decorations: DecorationSet;
+    constructor(view: EditorView) {
+      this.decorations = Decoration.set([]);
+      console.log("view", view.state.changes());
+    }
+    update(update: ViewUpdate) {
+      if (update.docChanged) {
+        // TODO
+      }
+    }
+  }
+);
+
 const Editor = () => {
   useEffect(() => {
     const editorState = EditorState.create({
       doc: "# Heading 1\n\n## Heading 2\n",
       extensions: [
+        addHeading12md,
         history(),
-        markdown({
-          extensions: [{}],
-        }),
+        markdown(),
         keymap.of([...defaultKeymap, ...historyKeymap, ...markdownKeymap]),
       ],
     });
