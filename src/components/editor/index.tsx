95d929341fcd360bbb568c74e006a3eff2aaa4e4
ruofei <rf.wangchn@foxmail.com>
2022-03-20 00:40:21 +0800

    style: 编辑器添加 md 类名

--- src/components/editor/index.tsx
+++ src/components/editor/index.tsx
@@ -1,51 +1,35 @@
 import { useEffect } from "react";
 import { EditorState } from "@codemirror/state";
-import {
-  Decoration,
-  DecorationSet,
-  EditorView,
-  keymap,
-  ViewPlugin,
-  ViewUpdate,
-} from "@codemirror/view";
-import { syntaxTree } from "@codemirror/language";
 import { defaultKeymap } from "@codemirror/commands";
+import { EditorView, keymap } from "@codemirror/view";
 import { history, historyKeymap } from "@codemirror/history";
-import { defaultHighlightStyle } from "@codemirror/highlight";
 import { markdown, markdownKeymap } from "@codemirror/lang-markdown";
+import {
+  defaultHighlightStyle,
+  HighlightStyle,
+  tags,
+} from "@codemirror/highlight";
 
 import styles from "./index.module.scss";
 
-const addHeading12md = ViewPlugin.fromClass(
-  class {
-    decorations: DecorationSet;
-    constructor(view: EditorView) {
-      this.decorations = Decoration.set([]);
-      for (const range of view.visibleRanges) {
-        syntaxTree(view.state).iterate({
-          from: range.from,
-          to: range.to,
-          enter: function (type, from, to) {
-            console.log(type, from, to);
-          },
-        });
-      }
-    }
-    update(update: ViewUpdate) {
-      if (update.docChanged) {
-        // TODO
-      }
-    }
-  }
-);
+const mdHighlightStyle = HighlightStyle.define([
+  { tag: tags.heading, class: "heading" },
+  { tag: tags.heading1, class: "heading-1" },
+  { tag: tags.heading2, class: "heading-2" },
+  { tag: tags.heading3, class: "heading-3" },
+  { tag: tags.heading4, class: "heading-4" },
+  { tag: tags.heading5, class: "heading-5" },
+  { tag: tags.heading6, class: "heading-6" },
+]);
 
 const Editor = () => {
   useEffect(() => {
     const editorState = EditorState.create({
       doc: "# Heading 1\n\n## Heading 2\n",
       extensions: [
-        addHeading12md,
+        mdHighlightStyle,
         defaultHighlightStyle.fallback,
+        defaultHighlightStyle.extension,
         history(),
         markdown(),
         keymap.of([...defaultKeymap, ...historyKeymap, ...markdownKeymap]),
