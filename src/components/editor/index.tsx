bf7fa273085397f788a0f01226aab1ad261d980a
ruofei <rf.wangchn@foxmail.com>
2022-03-20 12:13:04 +0800

    feat: 动态添加类名

--- src/components/editor/index.tsx
+++ src/components/editor/index.tsx
@@ -1,9 +1,17 @@
 import { useEffect } from "react";
 import { EditorState } from "@codemirror/state";
 import { defaultKeymap } from "@codemirror/commands";
-import { EditorView, keymap } from "@codemirror/view";
+import {
+  Decoration,
+  DecorationSet,
+  EditorView,
+  keymap,
+  ViewPlugin,
+  ViewUpdate,
+} from "@codemirror/view";
 import { history, historyKeymap } from "@codemirror/history";
 import { markdown, markdownKeymap } from "@codemirror/lang-markdown";
+import { RangeSetBuilder } from "@codemirror/rangeset";
 import {
   defaultHighlightStyle,
   HighlightStyle,
@@ -11,15 +19,17 @@ import {
 } from "@codemirror/highlight";
 
 import styles from "./index.module.scss";
+import { Line } from "@codemirror/text";
+import { syntaxTree } from "@codemirror/language";
 
 const mdHighlightStyle = HighlightStyle.define([
-  { tag: tags.heading, class: "heading" },
-  { tag: tags.heading1, class: "heading-1" },
-  { tag: tags.heading2, class: "heading-2" },
-  { tag: tags.heading3, class: "heading-3" },
-  { tag: tags.heading4, class: "heading-4" },
-  { tag: tags.heading5, class: "heading-5" },
-  { tag: tags.heading6, class: "heading-6" },
+  { tag: tags.heading, class: "cm-heading" },
+  { tag: tags.heading1, class: "cm-heading-1" },
+  { tag: tags.heading2, class: "cm-heading-2" },
+  { tag: tags.heading3, class: "cm-heading-3" },
+  { tag: tags.heading4, class: "cm-heading-4" },
+  { tag: tags.heading5, class: "cm-heading-5" },
+  { tag: tags.heading6, class: "cm-heading-6" },
 ]);
 
 const Editor = () => {
@@ -27,6 +37,52 @@ const Editor = () => {
     const editorState = EditorState.create({
       doc: "# Heading 1\n\n## Heading 2\n",
       extensions: [
+        ViewPlugin.fromClass(
+          class {
+            decorations: DecorationSet;
+            constructor(view: EditorView) {
+              this.decorations = this.buildDeco(view);
+            }
+            update(update: ViewUpdate) {
+              if (update.docChanged || update.selectionSet) {
+                this.decorations = this.buildDeco(update.view);
+              }
+            }
+            buildDeco(view: EditorView) {
+              const builder = new RangeSetBuilder<Decoration>();
+              view.viewportLines((line) => {
+                const lineObj = view.state.doc.lineAt(line.from);
+                syntaxTree(view.state).iterate({
+                  from: lineObj.from,
+                  to: lineObj.to,
+                  enter: function (type, from, to) {
+                    if (lineObj.from === from && lineObj.to === to) {
+                      if (type.name === "ATXHeading1") {
+                        console.log("hhh");
+                        builder.add(
+                          line.from,
+                          line.from,
+                          Decoration.line({ class: "cm-heading-1" })
+                        );
+                      }
+                      if (type.name === "ATXHeading2") {
+                        builder.add(
+                          line.from,
+                          line.from,
+                          Decoration.line({ class: "cm-heading-2" })
+                        );
+                      }
+                    }
+                  },
+                });
+              });
+              return builder.finish();
+            }
+          },
+          {
+            decorations: (v) => v.decorations,
+          }
+        ),
         mdHighlightStyle,
         defaultHighlightStyle.fallback,
         defaultHighlightStyle.extension,
